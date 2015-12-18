612f4e3d1f9885ead535c222bbce38b7ac144a40
Allie Curry <allie516@gmail.com>
2015-12-18 14:08:13 -0600

    Added check to stop recording on fragment pause

--- app/src/main/java/com/windowmirror/android/controller/fragment/AudioRecordFragment.java
+++ app/src/main/java/com/windowmirror/android/controller/fragment/AudioRecordFragment.java
@@ -70,6 +70,11 @@ public class AudioRecordFragment extends Fragment implements AudioRecorder.Audio
 
     @Override
     public void onPause() {
+        if (isRecording) {
+            recordButton.setSelected(false);
+            stopRecording();
+            isRecording = false;
+        }
         super.onPause();
         if (soundEffectPlayer != null) {
             soundEffectPlayer.release();
