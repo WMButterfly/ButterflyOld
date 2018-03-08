43d02313e1d04ddfef3fe9dd6b9833d4515abe93
Allie Curry <allie@starsky.co>
2018-03-07 19:08:05 -0600

    Fix get recordings API config

--- app/src/main/java/com/windowmirror/android/service/BackendApi.java
+++ app/src/main/java/com/windowmirror/android/service/BackendApi.java
@@ -10,13 +10,14 @@ import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface BackendApi {
 
-    @PATCH("/recording")
+    @GET("/recording")
     Call<List<Recording>> getAllRecordings();
 
     @PATCH("/recording/{uuid}")
