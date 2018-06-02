fff684246407279224245ff3b0a28f6b23abea0c
Robert C. Wafle <robert@wafle.com>
2018-06-02 16:00:49 -0500

    removed always listening feature

--- app/src/main/java/com/windowmirror/android/controller/fragment/SettingsFragment.java
+++ app/src/main/java/com/windowmirror/android/controller/fragment/SettingsFragment.java
@@ -19,15 +19,19 @@ import com.windowmirror.android.util.LocalPrefs;
 public class SettingsFragment extends Fragment implements CompoundButton.OnCheckedChangeListener {
     public static final String TAG = SettingsFragment.class.getSimpleName();
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View v = inflater.inflate(R.layout.fragment_settings, container, false);
+        /* commented out to remove always listening
         final CompoundButton serviceCheckBox = (CompoundButton) v.findViewById(R.id.service_checkbox);
         serviceCheckBox.setChecked(LocalPrefs.getIsBackgroundService(getContext()));
         serviceCheckBox.setOnCheckedChangeListener(this);
+        */
         return v;
     }
+
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
