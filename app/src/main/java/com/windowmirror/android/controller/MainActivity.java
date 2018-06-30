268a30f7289daa948c17cf735b26badbede4be53
Robert C. Wafle <robert@wafle.com>
2018-06-29 21:16:13 -0500

    wip

--- app/src/main/java/com/windowmirror/android/controller/MainActivity.java
+++ app/src/main/java/com/windowmirror/android/controller/MainActivity.java
@@ -139,10 +139,50 @@ public class MainActivity extends FragmentActivity implements
         });
         Log.d ("butterfly", "inside onCreate!");
         Intent intent = getIntent();
+
+        this.handleIntent(intent);
+    }
+
+    void handleSendText(Intent intent) {
+        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+        if (sharedText != null) {
+            Log.d("butterfly", "sharedText: "+ sharedText);
+            final Entry entry = this.createEntry(sharedText);
+            //showRecordingFragment(true);
+            Log.d ("butterfly", "created entry.  entry.getFullTranscription():" + entry.getFullTranscription());
+            this.CreateEntry(entry);
+        } else {
+            Log.d("butterfly", "sharedText was null!?!?!?");
+        }
+    }
+
+    void handleSendImage(Intent intent) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            // Update UI to reflect image being shared
+        }
+    }
+
+    void handleSendMultipleImages(Intent intent) {
+        ArrayList<Uri> imageUris = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM);
+        if (imageUris != null) {
+            // Update UI to reflect multiple images being shared
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        this.handleIntent(intent);
+    }
+
+    protected void handleIntent(Intent intent) {
         String action = intent.getAction();
         String type = intent.getType();
+        Log.d ("butterfly", "inside onNewIntent");
         if (intent != null) {
-            Log.d ("butterfly", "started with intent. Action=" + action + " Type="+type + ".");
+            Log.d ("butterfly", "Started With Intent!! Action=" + action + " Type="+type + ".");
             dumpIntent(intent);
             if (NoteIntents.ACTION_CREATE_NOTE.equals(action)) {
                 Log.d ("butterfly", "started with intent to record!!");
@@ -154,32 +194,29 @@ public class MainActivity extends FragmentActivity implements
 
                 this.CreateEntry(entry);
                 //showRecordingFragment(true);
+            } else if (Intent.ACTION_SEND.equals(action) && type != null) {
+                Log.d ("butterfly", "Handling ACTION_SEND intent!");
+                if ("text/plain".equals(type)) {
+                    Log.d ("butterfly", "Type: text/plain!");
+                    handleSendText(intent); // Handle text being sent
+                } else if (type.startsWith("image/")) {
+                    Log.d ("butterfly", "Type: image!");
+                    handleSendImage(intent); // Handle single image being sent
+                }
+            } else if (Intent.ACTION_SEND_MULTIPLE.equals(action) && type != null) {
+                if (type.startsWith("image/")) {
+                    handleSendMultipleImages(intent); // Handle multiple images being sent
+                }
+            } else if(Intent.ACTION_MAIN.equals(action)){
+                Log.d ("butterfly", "Launched regularly!");
+            } else {
+                // Handle other intents, such as being started from the home screen
+                Log.d ("butterfly", "Launched with unexpected intent. ");
             }
         } else {
             Log.d ("butterfly", "started without intent.");
         }
     }
-
-    @Override
-    protected void onNewIntent(Intent intent)
-    {
-        super.onNewIntent(intent);
-        Log.d ("butterfly", "inside onNewIntent");
-        String action = intent.getAction();
-        String type = intent.getType();
-        if (intent != null) {
-            Log.d ("butterfly", "started with intent. Action=" + action + " Type="+type + ".");
-            if (NoteIntents.ACTION_CREATE_NOTE.equals(action)) {
-                Log.d ("butterfly", "started with intent to record!");
-                final Entry entry = this.createEntry("test entry!");
-                //showRecordingFragment(true);
-                Log.d ("butterfly", "created entry.  entry.getFullTranscription():" + entry.getFullTranscription());
-            }
-        } else {
-            Log.d ("butterfly", "started without intent.");
-        }
-    }
-
     //TODO:  Remove this copy paste uglyness!
     private Entry createEntry(String fullTranscription) {
         Log.d ("butterfly", "Creating Entry: "+ fullTranscription);
